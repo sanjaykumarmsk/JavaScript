bdc3fcf41869cc508796bee8bf052fc014d49716
sanjaykumarmsk <129680092+sanjaykumarmsk@users.noreply.github.com>
2024-01-15 02:59:49 +0530

    Operators in JS

---
+++ 04_index.js
@@ -0,0 +1,35 @@
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+// ********************* Operations ***************
+
+let value= 3
+let nagValue = -value
+console.log(nagValue)
+
+let str1 = "hello";
+let str2 = "sanjay";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1"+"2")
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"3")
+
+
+console.log(true)
+console.log(+true)
+
+let num1, num2, num3;
+num1=num2=num3=1+1
+
+let gameChanger = 100;
+// gameChanger++;
+// console.log(gameChanger)
+++gameChanger;
+console.log(gameChanger);
