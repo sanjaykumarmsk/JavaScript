1cc9977c52cc1381b8123d86cdc689e1f238946a
sanjaykumarmsk <129680092+sanjaykumarmsk@users.noreply.github.com>
2024-01-23 00:44:31 +0530

    intial Commit

---
+++ iteration_05/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5){
+//         console.log(`Detected 5`);
+//         break;
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
